c5c88eca649b0dfaab8cb081a1affa98116fe6e3
Juan Daniel Retrepo Zapata <Juan-zap98@hotmail.com>
2026-08-07 16:23:41 -0500

    Update to Exercise 01 and creation of the ConsoleExtension class

---
+++ Shared/ConsoleExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared;
+
+public static class ConsoleExtension
+{
+    public static int GetInt(string message)
+    {
+        Console.Write(message);
+        var numberString = Console.ReadLine();
+        if (int.TryParse(numberString, out numberInt))
+        {
+            return numberInt;
+        }
+        return 0;
+    }
+}
