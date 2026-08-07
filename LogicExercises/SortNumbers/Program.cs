a3f6d34a3dfa28618adb7ec1dcee8f81c00a9556
Juan Daniel Retrepo Zapata <Juan-zap98@hotmail.com>
2026-08-07 17:42:12 -0500

    Solving Exercises 02 (The Largest Number Is) and 03 (Order of Numbers)

---
+++ LogicExercises/SortNumbers/Program.cs
@@ -0,0 +1,62 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+do
+{
+    Console.WriteLine("Ingrese tres números diferentes...");
+    var a = ConsoleExtension.GetInt("Ingrese el primer número: ");
+    var b = ConsoleExtension.GetInt("Ingrese el segundo número: ");
+    if (a == b)
+    {
+        Console.WriteLine("Deben ser diferentes, vuelve a empezar");
+        continue;
+    }
+   
+    var c = ConsoleExtension.GetInt("Ingrese el tercer número: ");
+    if (b == c || c == a)
+    {
+        Console.WriteLine("Deben ser diferentes, vuelve a empezar");
+        continue;
+    }
+    if (a > b && a > c)
+    {
+        if(b > c)
+        {
+            Console.WriteLine($"El número mayor es: {a}, El número del medio es: {b}, El número menor es: {c}");
+        }
+        else
+        {
+            Console.WriteLine($"El número mayor es: {a}, El número del medio es: {c}, El número menor es: {b}");
+        }
+    }
+    else if (b > a && b > c)
+    {
+        if (a > c)
+        {
+            Console.WriteLine($"El número mayor es: {b}, El número del medio es: {a}, El número menor es: {c}");
+        }
+        else
+        {
+            Console.WriteLine($"El número mayor es: {b}, El número del medio es: {a}, El número menor es: {c}");
+        }
+    }
+    else
+    {
+        if (a > b)
+        {
+            Console.WriteLine($"El número mayor es: {c}, El número del medio es: {a}, El número menor es: {b}");
+        }
+        else
+        {
+            Console.WriteLine($"El número mayor es: {c}, El número del medio es: {b}, El número menor es: {a}");
+        }
+    }
+    
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Game Over.");
---
+++ LogicExercises/TheGreatestNumberIs/Program.cs
@@ -0,0 +1,30 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+do
+{
+    var a = ConsoleExtension.GetInt("Ingrese el primer número: ");
+    var b = ConsoleExtension.GetInt("Ingrese el segundo número: ");
+    var c = ConsoleExtension.GetInt("Ingrese el tercer número: ");
+
+    if (a > b && a > c)
+    {
+        Console.WriteLine($"El número mayor es el: {a}");
+    }
+    else if (b>a && b>c)
+    {
+        Console.WriteLine($"El número mayor es el: {b}");
+    }
+    else
+    {
+        Console.WriteLine($"El número mayor es el: {c}");
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Game Over.");
