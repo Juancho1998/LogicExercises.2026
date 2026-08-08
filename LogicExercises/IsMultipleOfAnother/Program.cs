5eb33043a4821b85b5b869ee7677035e90c95863
Juan Daniel Retrepo Zapata <Juan-zap98@hotmail.com>
2026-08-08 17:31:38 -0500

    Solution exercise 04 Is Multiple of another number

---
+++ LogicExercises/IsMultipleOfAnother/Program.cs
@@ -0,0 +1,17 @@
+﻿using Shared;
+
+do
+{
+    Console.WriteLine("Ingrese 2 números: ");
+    var a = ConsoleExtension.GetInt("Ingrese el primer número: ");
+    var b = ConsoleExtension.GetInt("Ingrese el segundo número: ");
+
+    if (a % b == 0)
+    {
+        Console.WriteLine($"El número: {b}, es múltiplo del: {a}");
+    }
+    else
+    {
+        Console.WriteLine($"El número: {b}, no es múltiplo del: {a}");
+    }
+} while (true);
